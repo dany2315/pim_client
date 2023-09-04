4603f7c1f9c6aa38052d7246254eb274776838ef
Dany serfaty <davidserfaty2315@gmail.com>
2023-09-04 12:54:57 +0300

    all refresh marche et fait l'importation +filtrer champs vide + renregistrer en fonction dynamique

--- src/components/Fournisseurs/Bloc/FournisseurFtp/index.js
+++ src/components/Fournisseurs/Bloc/FournisseurFtp/index.js
@@ -1,19 +1,19 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect,useContext } from "react";
 import { Grid, Typography, Button } from "@mui/material";
-import RefreshIcon from '@mui/icons-material/Refresh';
+import RefreshIcon from "@mui/icons-material/Refresh";
 import api from "../../../../utils/Axios";
-import Papa from "papaparse";
-//import context
+import { HandleRefreshFtp } from "../../../logique/RefreshImportFtp";
 import { SnackbarContext } from "../../../../context/snackbarContext";
 import { LoadingContext } from "../../../../context/loadingContext";
 
-const FournisseurFtp = ({ fournisseur }) => {
 
-  const [keyNames, setKeyNames] = useState([]);
-  const [time, setTime] = useState(false);
-  const collect = fournisseur.collectionName;
+
+const FournisseurFtp = ({ fournisseur }) => {
   const { showSnackbar } = useContext(SnackbarContext);
   const { showLoading, hideLoading } = useContext(LoadingContext);
+  const [time, setTime] = useState("");
+  const collect = fournisseur.collectionName;
+
 
   useEffect(() => {
     GetTimeMaj();
@@ -22,106 +22,25 @@ const FournisseurFtp = ({ fournisseur }) => {
   const GetTimeMaj = async () => {
     try {
       const reponse = await api.get(`/fournisseur/ftp/${collect}`);
-      setTime(reponse.data);
+      const date = new Date(reponse.data);
+      const jour = date.getDate();
+      const mois = date.getMonth() + 1; // Les mois commencent à 0, donc ajoutez 1
+      const annee = date.getFullYear();
+      const heures = date.getHours();
+      const minutes = date.getMinutes();
+      const dateFormatee = `${jour.toString().padStart(2, "0")}/${mois
+        .toString()
+        .padStart(2, "0")}/${annee.toString().slice(2)} ${heures
+        .toString()
+        .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+      setTime(dateFormatee);
       console.log(`reponse data ${collect} `, reponse.data);
     } catch (error) {
       console.log("erreur lors de la recup plein ", error);
     }
   };
 
-  const importFtp = async () => {
 
-    if (fournisseur.host && fournisseur.username && fournisseur.password && fournisseur.filName) {
-      try {
-        showLoading()
-        console.log("value : ",  fournisseur.password, fournisseur.username , fournisseur.host , fournisseur.filNames);
-        const response = await api.post("/fournisseur/ftp", {
-          urlFtp: fournisseur.host,
-          nomUtilis: fournisseur.username,
-          pass: fournisseur.password,
-          nameFile: fournisseur.filName,
-        });
-        hideLoading()
-        showSnackbar("Importation par FTP reussi ","success")
-        console.log("reponse ftp", response.data);
-        return  response.data
-      } catch (error) {
-        console.log("error ftp", error);
-        hideLoading()
-        showSnackbar("Importation par FTP echouer !!", "error")
-      }
-    } else {
-    }
-    
-  };
-
-  const handleFile = (data) => {
-  
-    const dataNoFilter = data;
-    // Filtrer les objets où 'sku' est vide
-    let filteredData = [];
-    if (dataNoFilter) {
-      filteredData = dataNoFilter.filter((item) => {
-        for (let key in item) {
-          if (item.hasOwnProperty(key) && item[key] === "") {
-            return false;
-          }
-        }
-        return true;
-      });
-    }
-    //nom des proprieter
-    const propertyNames =
-      filteredData.length > 0 ? Object.keys(filteredData[0]) : [];
-    setKeyNames(propertyNames);
-
-    console.log("donne filtrer :", filteredData);
-    console.log("keyNames :", propertyNames);
-return filteredData
-};
-
-  const handleRefreshFtp = async () => {
-    const data = await importFtp()
-    const updatedData = handleFile(data);
-    showLoading();
-    console.log("liste key names ", keyNames);
-    console.log("liste fieldNames : ", fournisseur.fieldNames);
-    console.log("data : ", updatedData);
-    //Sortir le data final en enlevant les champs et les valeur des
-    //articles indispenssable et garder que les autre
-    const updateDatascop = updatedData.map((item) => {
-      const updatedItem = {};
-      Object.keys(item).forEach((key, index) => {
-        const updatedKey = fournisseur.fieldNames[index];
-        if (updatedKey) {
-          updatedItem[updatedKey] = item[key];
-        }
-      });
-      fournisseur.fieldNames.forEach((updatedKey, index) => {
-        if (updatedKey === "non_necessaire") {
-          delete updatedItem[index]; // Supprimer la clé
-          delete updatedItem[updatedKey]; // Supprimer la valeur correspondante
-        }
-      });
-      return updatedItem;
-    });
-
-    try {
-      const response = await api.post("/fournisseur/ftp/refreshFournisseurFtp", {
-        data: updateDatascop,
-        collectionName: fournisseur.collectionName,
-      });
-      console.log("succes resauvgarde ", response.data);
-      hideLoading();
-      showSnackbar("Mise a niveau effectuer !", "success");
-    } catch (error) {
-      hideLoading();
-      showSnackbar("Mise a niveau echouer !", "error");
-      console.error("Erreur lors de la resauvegarde des données :", error);
-    }
-  };
-
-  
   return (
     <>
       <Grid
@@ -150,24 +69,23 @@ return filteredData
         </Grid>
 
         <Grid item xs={4} sx={{ borderLeft: "solid 1px", textAlign: "center" }}>
-          <Button sx={{
-            w:0.5,
-            pr:0,
-            color: 'green', // Couleur du texte en blanc
-            bgcolor: 'white', // Couleur de fond en vert
-            boxShadow:"1px 2px 2px 1px",
-            textAlign:"center",
-            '&:hover': {
-              bgcolor: 'darkgreen', // Couleur de fond au survol en vert foncé
-              color:"white",
-            },
-          }}
+          <Button
+            sx={{
+              w: 0.5,
+              pr: 0,
+              color: "green", // Couleur du texte en blanc
+              bgcolor: "white", // Couleur de fond en vert
+              boxShadow: "1px 2px 2px 1px",
+              textAlign: "center",
+              "&:hover": {
+                bgcolor: "darkgreen", // Couleur de fond au survol en vert foncé
+                color: "white",
+              },
+            }}
             startIcon={<RefreshIcon />} // Utilisez l'icône de rafraîchissement
             size="small" // Vous pouvez ajuster la taille selon vos besoins
-            onClick={handleRefreshFtp}
-          >
-           
-          </Button>
+            onClick={async()=>(await HandleRefreshFtp(fournisseur ,showSnackbar ,showLoading, hideLoading),GetTimeMaj())}
+          ></Button>
         </Grid>
         <Grid item xs={4} sx={{ borderLeft: "solid 1px", textAlign: "center" }}>
           <Typography variant="h8" fontFamily={"cursive"}>
@@ -178,4 +96,4 @@ return filteredData
     </>
   );
 };
-export default FournisseurFtp;
+export default FournisseurFtp 
--- src/components/Fournisseurs/Bloc/index.js
+++ src/components/Fournisseurs/Bloc/index.js
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState , useContext} from "react";
 import api from "../../../utils/Axios";
 import { Grid, Typography } from "@mui/material";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
-import RefreshIcon from '@mui/icons-material/Refresh';
+import RefreshIcon from "@mui/icons-material/Refresh";
 import Fournisseur from "./Fournisseur";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faServer } from "@fortawesome/free-solid-svg-icons";
 import FournisseurFtp from "./FournisseurFtp";
+import { HandleRefreshFtp } from "../../logique/RefreshImportFtp";
+import { SnackbarContext } from "../../../context/snackbarContext"
+import { LoadingContext } from "../../../context/loadingContext";
 
 const Bloc = (props) => {
+  const { showSnackbar } = useContext(SnackbarContext);
+  const { showLoading, hideLoading } = useContext(LoadingContext);
   const categorie = props.categorie;
   const [fournisseurs, setfournisseurs] = useState([]);
 
@@ -29,42 +34,45 @@ const Bloc = (props) => {
     fetchFournisseur();
   }, []);
 
+  
+  const refreshFournisseurAllFtp = async () => {
+      fournisseurs.map((fournisseur,index)=>
+       HandleRefreshFtp(fournisseur ,showSnackbar ,showLoading, hideLoading))
+  }
+
   return (
     <>
-<Grid
-container
-        direction="row"
-       >
+      <Grid container direction="row">
+        <Grid item xs={6}>
+          <Typography variant="h5" color={"#8eb8fb"} fontFamily={"cursive"}>
+            <FontAwesomeIcon icon={faServer} style={{ color: "#8eb8fb" }} />{" "}
+            {categorie.name}
+          </Typography>
+        </Grid>
+        {categorie.name == "ftp" ? (
+          <Grid item xs={6} textAlign={"right"}>
+            <Button
+              sx={{
+                w: 0.5,
+                pr: 0,
+                color: "green", // Couleur du texte en blanc
+                bgcolor: "white", // Couleur de fond en vert
+                boxShadow: "1px 2px 2px 1px",
+                textAlign: "center",
+                "&:hover": {
+                  bgcolor: "darkgreen", // Couleur de fond au survol en vert foncé
+                  color: "white",
+                },
+              }}
+              onClick={refreshFournisseurAllFtp}
+              startIcon={<RefreshIcon />} // Utilisez l'icône de rafraîchissement
+              size="small" // Vous pouvez ajuster la taille selon vos besoins
+              // Autres props MUI peuvent être ajoutés ici
+            ></Button>
+          </Grid>
 
-    <Grid item xs={6}>
-        <Typography variant="h5" color={"#8eb8fb"} fontFamily={"cursive"}>
-          <FontAwesomeIcon icon={faServer} style={{ color: "#8eb8fb" }} />{" "}
-          {categorie.name}
-        </Typography>
-    </Grid>
-    {categorie.name == "ftp" ?
-    <Grid item xs={6} textAlign={"right"}>
-      
-    <Button sx={{
-            w:0.5,
-            pr:0,
-            color: 'green', // Couleur du texte en blanc
-            bgcolor: 'white', // Couleur de fond en vert
-            boxShadow:"1px 2px 2px 1px",
-            textAlign:"center",
-            '&:hover': {
-              bgcolor: 'darkgreen', // Couleur de fond au survol en vert foncé
-              color:"white",
-            },
-          }}
-            startIcon={<RefreshIcon />} // Utilisez l'icône de rafraîchissement
-            size="small" // Vous pouvez ajuster la taille selon vos besoins
-            // Autres props MUI peuvent être ajoutés ici
-          >
-           
-          </Button>
-    </Grid>:null}
-    </Grid>
+        ) : null}
+      </Grid>
       {console.log("cat", categorie)}
       <Grid
         overflow={"auto"}
