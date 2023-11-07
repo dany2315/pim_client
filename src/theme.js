6ecedb6d4e56b0f3ce198737211cdf3a63d6e5a9
Dany serfaty <davidserfaty2315@gmail.com>
2023-11-07 11:22:10 +0200

    service worker + pwa +theme +talwindcss

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#a78d60',
+      
+      dark:'#EFCA8A',
+    },
+    secondary: {
+      main: '#b26500',
+    },
+  },
+});
+
+export default theme
